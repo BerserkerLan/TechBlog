8ef749e279f921645379704440414afee7949166
BerserkerLan <rusab36@gmail.com>
2023-12-12 12:00:50 +0500

    wrote first tech blog of mac os bad, removed other blogs

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'PandaTech',
+  subtitle: 'A tech blog where I write opinions and helpful articless',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Heavily based on Urara Sveltekit',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    avatar: '/assets/pixel_art_typing_panda.jpg',
+    name: 'CodePanda',
+    status: '💻',
+    bio: 'A windows user that uses Linux periodically.'
   },
   themeColor: '#3D4451'
 }
