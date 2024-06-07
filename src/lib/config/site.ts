aedd1411ce4a51af1979fdd45104d5b035df9a43
Rusab <rusab36@gmail.com>
2024-06-07 16:49:41 +0100

    changed avatar picture to a test png

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,14 +2,14 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: 'https://berserkerlan.github.io/techblog/',
+  domain: 'https://berserkerlan.github.io',
   title: 'Rusab Tech Blog',
   subtitle: 'A tech blog where I write opinions and helpful articles',
   lang: 'en-US',
   description: 'A tech blog for personal opinions and articles',
   author: {
-    avatar: '/assets/pixel_art_typing_panda.jpg',
-    name: 'CodePanda',
+    avatar: '/assets/any@512.png',
+    name: 'Rusab',
     status: '💻',
     bio: 'A senior software engineer who loves to make fun projects.'
   },
